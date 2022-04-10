9f58fb919538b8bbf8b1a4c6d77ec83383f24631
ltk <dlxorb1223@naver.com>
2022-04-10 22:49:28 +0900

    오타수정

--- js/ajax.js
+++ js/ajax.js
@@ -13,7 +13,7 @@ $(function(){
     
         $.ajax({
             type : "GET",            // HTTP method type(GET, POST) 형식이다.
-            url : "https://ltk-92.github.io/ltkpot/js/"+dataName+".json",      // 컨트롤러에서 대기중인 URL 주소이다.         
+            url : "https://ltk-92.github.io/ltkpo/js/"+dataName+".json",      // 컨트롤러에서 대기중인 URL 주소이다.         
             dataType: "json", // Json 형식의 데이터이다.
             success : function(info){ // 비동기통신의 성공일경우 success콜백으로 들어옵니다. 'res'는 응답받은 데이터이다.
                 // 응답코드 > 0000
