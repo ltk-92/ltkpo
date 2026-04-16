9c6a0f43946d7ee679ee16a90483917af81936c7
ltk <dlxorb1223@naver.com>
2026-04-16 17:00:29 +0900

    휠 이벤트 삭제, 다이어리 메뉴 삭제, json 다이어리 부분 삭제

--- js/custom.js
+++ js/custom.js
@@ -28,55 +28,56 @@
 	});
     
     //마우스 휠 동작시 아래로 스크롤 이동
-	$(window).scroll(function(){
-		logodel();
-		//var sectionTop = parseInt($("#profile-wrap").offset().top);
-		//console.log("sectionTop"+sectionTop);
-		//if($(window).scrollTop()==sectionTop){
-		//	$("#profile-wrap").children().fadeOut();
+	// $(window).scroll(function(){
+	// 	logodel();
+	// 	//var sectionTop = parseInt($("#profile-wrap").offset().top);
+	// 	//console.log("sectionTop"+sectionTop);
+	// 	//if($(window).scrollTop()==sectionTop){
+	// 	//	$("#profile-wrap").children().fadeOut();
 	
 			
-		//};
-        var size = $('section').size();//갯수
-		$('section').on('mousewheel',function(event,delta){
-			if(delta>0 && $(this).index()>=1){
-				var present = $(this).prev();
-				pageshow(present);
-				var presentTop = Math.ceil(present.offset().top);
-				$('html,body').stop().animate({'scrollTop':presentTop},2000,'easeOutExpo')
-				 //if(delta>0 && $(this).index()==1){
-					//clearInterval(textset);
-					//slideText();
-				//};
-			}else if(delta<0 && $(this).index()<3){
-				var present = $(this).next();
-				pageshow(present);
-				var presentTop = Math.ceil(present.offset().top);
-				$('html,body').stop().animate({'scrollTop':presentTop},2000,'easeOutExpo')
-				//if(delta<0 && $(this).index()==0){
-					//clearInterval(textset);
-				//};
-			}else if(delta<0 && $(this).index()==3){
-				var present = $("#footer-wrap");
-				pageshow(present);
-				var presentTop = Math.ceil(present.offset().top);
-				$('html,body').stop().animate({'scrollTop':presentTop},2000,'easeOutExpo')
-			};
+	// 	//};
+    //     var size = $('section').size();//갯수
+	// 	$('section').on('mousewheel',function(event,delta){
+	// 		if(delta>0 && $(this).index()>=1){
+	// 			var present = $(this).prev();
+	// 			pageshow(present);
+	// 			var presentTop = Math.ceil(present.offset().top);
+	// 			$('html,body').stop().animate({'scrollTop':presentTop},2000,'easeOutExpo')
+	// 			 //if(delta>0 && $(this).index()==1){
+	// 				//clearInterval(textset);
+	// 				//slideText();
+	// 			//};
+	// 		}else if(delta<0 && $(this).index()<3){
+	// 			var present = $(this).next();
+	// 			pageshow(present);
+	// 			var presentTop = Math.ceil(present.offset().top);
+	// 			$('html,body').stop().animate({'scrollTop':presentTop},2000,'easeOutExpo')
+	// 			//if(delta<0 && $(this).index()==0){
+	// 				//clearInterval(textset);
+	// 			//};
+	// 		}else if(delta<0 && $(this).index()==3){
+	// 			var present = $("#footer-wrap");
+	// 			pageshow(present);
+	// 			var presentTop = Math.ceil(present.offset().top);
+	// 			$('html,body').stop().animate({'scrollTop':presentTop},2000,'easeOutExpo')
+	// 		};
 			
 	
-		});
-		$('#footer-wrap').on('mousewheel',function(event,delta){
-            if(delta>0){
-				var present = $("section").eq(3);
-				pageshow(present);
-				var presentTop = present.offset().top;
-				$('html,body').stop().animate({'scrollTop':presentTop},2000,'easeOutExpo');
-			};
+	// 	});
+	// 	$('#footer-wrap').on('mousewheel',function(event,delta){
+    //         if(delta>0){
+	// 			var present = $("section").eq(3);
+	// 			pageshow(present);
+	// 			var presentTop = present.offset().top;
+	// 			$('html,body').stop().animate({'scrollTop':presentTop},2000,'easeOutExpo');
+	// 		};
 		
-        });
-	});		
-    //end
-	 console.log("test");
+    //     });
+	// });		
+    // //end
+	//  console.log("test");
+
 	 function draw(max, classname, colorname1,colorname2){
 		var cnt=1;
 		 func1 = setInterval(donut,13);
