1ef1c0d523d23738294d70389522c7c127f21df5
ltk <dlxorb1223@naver.com>
2026-04-26 19:44:17 +0900

    swiper.js 연동

--- js/slide.js
+++ js/slide.js
@@ -1,31 +1,23 @@
-  $(function(){
+//   $(function(){
       
-        /*$('section').on('mousewheel',function(event,delta){
-            if($(".greeting-list").hasClass("pageshow")){
-                slideText(0);
-            }else if($(".greeting-list").hasClass("pagehide")){
-                clearInterval(textmotion);
-            };
-        });*/
-
-    //End Each
+//     //End Each
     
-    //화살표 버튼
-     $(".left-btn").click(function(){
-        var webList = $(".web-list");
-        $(".web-list>li").fadeOut();
-        $(".web-list>li:last").prependTo(webList);
-        $(".web-list>li").removeClass("web-on");
-        $(".web-list>li:last").addClass("web-on");
-        $(".web-on").fadeIn();
-    });
-    $(".right-btn").click(function(){
-        var webList = $(".web-list");
-        $(".web-list>li").fadeOut();
-        $(".web-list>li:first").appendTo(webList);
-        $(".web-list>li").removeClass("web-on");
-        $(".web-list>li:first").addClass("web-on");
-        $(".web-on").fadeIn();
+//     //화살표 버튼
+//      $(".left-btn").click(function(){
+//         var webList = $(".web-list");
+//         $(".web-list>li").fadeOut();
+//         $(".web-list>li:last").prependTo(webList);
+//         $(".web-list>li").removeClass("web-on");
+//         $(".web-list>li:last").addClass("web-on");
+//         $(".web-on").fadeIn();
+//     });
+//     $(".right-btn").click(function(){
+//         var webList = $(".web-list");
+//         $(".web-list>li").fadeOut();
+//         $(".web-list>li:first").appendTo(webList);
+//         $(".web-list>li").removeClass("web-on");
+//         $(".web-list>li:first").addClass("web-on");
+//         $(".web-on").fadeIn();
         
-    });
-}); //End jquery
+//     });
+// }); //End jquery
